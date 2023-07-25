7f8bc7e66548babdeeaf295ae2ef63df73d11b0f
Trantarius <leborjt@yahoo.com>
2023-07-24 21:53:32 -0400

    implemented scary orb

---
+++ Assets/ScaryOrb/ScaryOrb.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScaryOrb : MonoBehaviour
+{
+
+    public float speed=10.0f;
+    private Detector detector;
+    private Rigidbody body;
+    // Start is called before the first frame update
+    void Start()
+    {
+        detector=transform.GetChild(0).GetComponent<Detector>();
+        body=GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        List<GameObject> detected=detector.getVisible();
+
+        if(detected.Count==0){
+            return;
+        }
+
+        Vector3 target=detected[0].transform.position;
+        Vector3 to_target=(target-transform.position).normalized;
+
+        Vector3 torque=Vector3.Cross(to_target,Vector3.down)*speed;
+        body.AddTorque(torque);
+    }
+}
