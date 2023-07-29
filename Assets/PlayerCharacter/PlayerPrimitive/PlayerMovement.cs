ffd561f85a80bd190a4833854267a57a908394ec
RishiPrasath <73206126+RishiPrasath@users.noreply.github.com>
2023-07-29 15:02:21 +0800

    Basic Movement and Jump Implemented for Player
    - Content at PlayerScene
    - CameraMovement.csd to follow Player
    - PlayerMovement.cs for Move and Jump
    - Cube Designated as Floor

---
+++ Assets/PlayerCharacter/PlayerPrimitive/PlayerMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    private float move;
+
+    public float jumpForce;
+
+    public bool isJumping;
+    
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float move = Input.GetAxis("Horizontal"); // A/D or Left/Right
+        rb.velocity = new Vector3(move * speed, rb.velocity.y); // Move left/right
+
+        // Apply jump only if the player is grounded (not jumping)
+        if (Input.GetButtonDown("Jump") && !isJumping)
+        {
+            rb.AddForce(new Vector3(0, jumpForce, 0), ForceMode.Impulse); // Jump with upward force
+            isJumping = true;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        // Check if the player touches the "Floor" tagged object
+        if (collision.gameObject.CompareTag("Floor"))
+        {
+            isJumping = false; // Set isJumping to false when grounded
+        }
+    }
+}
