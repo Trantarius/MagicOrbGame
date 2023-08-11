1ae7f0714846bc5933a9a81e755982c086e65619
RishiPrasath <73206126+RishiPrasath@users.noreply.github.com>
2023-08-11 23:40:24 +0800

    Water Orb Prefab
    - Created a Prefab for the Water Orb
    - Included Material to to color it in water-like blue
    - RigidBody and Collssion code is added for the wall
    
    *Yet to be done*
    
    Collison with the Scary Orb

---
+++ Assets/WaterOrb/Scripts/WaterOrbBehavior.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterOrbBehavior : MonoBehaviour
+{
+    public float slowdownFactor = 0.5f; // Adjust this to control how much the orb slows down upon collision
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Wall"))
+        {
+            // Calculate the new velocity by reducing the current velocity based on the slowdownFactor
+            Vector3 newVelocity = rb.velocity * slowdownFactor;
+
+            // Apply the new velocity to the Rigidbody
+            rb.velocity = newVelocity;
+        }
+    }
+}
