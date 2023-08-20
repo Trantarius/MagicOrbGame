331751a0da525a341f0e759bcfd4f6e8f86a6f3c
Albin Morner <albin.morner@gmail.com>
2023-08-20 21:33:04 +0300

    Add lava. Add text messages

---
+++ Assets/Lava/Lava.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class Lava : MonoBehaviour
+{
+    void OnTriggerEnter(Collider col)
+    {
+        Debug.Log("1");
+        Debug.Log(col.gameObject.tag);
+        if (col.gameObject.tag == "WaterOrb" || col.gameObject.tag == "Player")
+        {
+            Debug.Log("2");
+            EventBus.RaiseOnLevelFailed();
+        }
+    }
+}
