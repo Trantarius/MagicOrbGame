f31abe25ce78c318d38ccab6a726469cc977939f
Albin Morner <albin.morner@gmail.com>
2023-08-20 17:28:11 +0300

    Add URP. Add draft of first level. Add pond. Add event bus.

---
+++ Assets/Scripts/EventBus.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class EventBus : MonoBehaviour
+{
+    public static EventBus singletonInstance;
+    public delegate void OnLevelCompleted();
+    public static event OnLevelCompleted onLevelCompleted;
+
+   
+    void Awake()
+    {
+        if (singletonInstance == null)
+        {
+            singletonInstance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+
+    public static void RaiseOnLevelCompleted() {
+        if (onLevelCompleted != null)
+        {
+            onLevelCompleted();
+        }
+    }
+}
---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    public static LevelManager singletonInstance;
+    void Awake()
+    {
+        if (singletonInstance == null)
+        {
+            singletonInstance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+    void OnEnable()
+    {
+        EventBus.onLevelCompleted += LoadLevel;
+    }
+
+    void OnDisable()
+    {
+        EventBus.onLevelCompleted -= LoadLevel;
+    }
+
+    public void LoadLevel()
+    {
+        var nextSceneIndex = (SceneManager.GetActiveScene().buildIndex + 1) % SceneManager.sceneCount;
+        StartCoroutine(loadLevel(nextSceneIndex));
+    }
+
+    private IEnumerator loadLevel(int nextSceneIndex)
+    {
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+}
