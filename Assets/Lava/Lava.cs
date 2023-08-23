dfd0f3eac9af344141f358e863bab1653131b93c
Albin Morner <albin.morner@gmail.com>
2023-08-23 13:33:27 +0300

    Add intro level. Add draft for scary orb level. Add UI text for completing/failing a level. Progress to next level on level completed

--- Assets/Lava/Lava.cs
+++ Assets/Lava/Lava.cs
@@ -4,11 +4,8 @@ public class Lava : MonoBehaviour
 {
     void OnTriggerEnter(Collider col)
     {
-        Debug.Log("1");
-        Debug.Log(col.gameObject.tag);
         if (col.gameObject.tag == "WaterOrb" || col.gameObject.tag == "Player")
         {
-            Debug.Log("2");
             EventBus.RaiseOnLevelFailed();
         }
     }
