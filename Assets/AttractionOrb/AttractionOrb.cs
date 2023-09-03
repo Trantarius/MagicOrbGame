4972a7df3fa2265ef3f688eda155701a3d861b38
RishiPrasath <73206126+RishiPrasath@users.noreply.github.com>
2023-09-03 22:29:23 +0800

    Updated logic on AttractionOrb

--- Assets/AttractionOrb/AttractionOrb.cs
+++ Assets/AttractionOrb/AttractionOrb.cs
@@ -6,20 +6,13 @@ public class AttractionOrb : MonoBehaviour
 {
     public float magneticForce = 10f; // Adjust this value to control the strength of the magnetic force
 
-    //Magnetic Force Audio Source
+    // Magnetic Force Audio Source
     public AudioSource magneticForceAudioSource;
 
-    void OnTriggerStay(Collider other)
+    private void OnTriggerStay(Collider other)
     {
         if (other.CompareTag("WaterOrb"))
         {
-            // Play the magnetic force audio clip on loop
-            magneticForceAudioSource.Play();
-            
-
-
-
-            Debug.Log("OnTriggerStay");
             // Calculate the force direction from the water orb to the attraction orb
             Vector3 forceDirection = transform.position - other.transform.position;
             forceDirection.Normalize();
@@ -27,6 +20,12 @@ public class AttractionOrb : MonoBehaviour
             // Apply the magnetic force to the water orb's Rigidbody
             Rigidbody waterOrbRb = other.GetComponent<Rigidbody>();
             waterOrbRb.AddForce(forceDirection * magneticForce * Time.fixedDeltaTime);
+
+            // Play the magnetic force audio clip on loop
+            if (!magneticForceAudioSource.isPlaying)
+            {
+                magneticForceAudioSource.Play();
+            }
         }
     }
 }
