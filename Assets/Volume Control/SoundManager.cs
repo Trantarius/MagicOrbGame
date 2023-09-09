0cbd63461b7f48fe7cffc974dd3d618358fef794
AnnaSivolodskaia <anna.sivolodskaia@gmail.com>
2023-09-09 15:35:28 +0200

    Volume control added + minor bug fixes

---
+++ Assets/Volume Control/SoundManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundManager : MonoBehaviour
+{
+    [SerializeField] Slider volumeSlider;
+
+     public void ChangeVolume()
+    {
+        AudioListener.volume = volumeSlider.value;
+    }
+
+}
